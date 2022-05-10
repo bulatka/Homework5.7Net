61614d65792aa6a16a06713b922e957ee62cc938
bulatka <305264@gmail.com>
2022-05-10 17:40:06 +0300

    First task complete!

--- src/main/java/ClientApp.java
+++ src/main/java/ClientApp.java
@@ -2,24 +2,18 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.net.ServerSocket;
 import java.net.Socket;
 
-public class ServerApp {
+public class ClientApp {
     public static void main(String[] args) {
+        String host = "localhost";
         int port = 8089;
 
-        try (ServerSocket server = new ServerSocket(port);
-             Socket socket = server.accept();
+        try (Socket socket = new Socket(host, port);
              PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
              BufferedReader in = new BufferedReader(
                      new InputStreamReader(socket.getInputStream()))) {
-            System.out.println("New connection accepted");
-
-            final String name = in.readLine();
-
-            out.println(String.format("Hi %s, your port is %d", name, socket.getPort()));
-
+            out.println("Netology");
         } catch (IOException e) {
             e.printStackTrace();
         }
