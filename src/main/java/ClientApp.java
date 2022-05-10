d0fc429efa9d901bee4049e9a3514981021fc8a0
bulatka <305264@gmail.com>
2022-05-10 20:32:03 +0300

    Second task complete!

--- src/main/java/ClientApp.java
+++ src/main/java/ClientApp.java
@@ -6,14 +6,37 @@ import java.net.Socket;
 
 public class ClientApp {
     public static void main(String[] args) {
-        String host = "localhost";
-        int port = 8089;
+        final String host = "netology.homework";
+        final int port = 8089;
+        final int age = 34;
+        final String name = "Bulatka";
+        final int choice = 5;
 
         try (Socket socket = new Socket(host, port);
              PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
              BufferedReader in = new BufferedReader(
                      new InputStreamReader(socket.getInputStream()))) {
-            out.println("Netology");
+            String resp = " ";
+            try {
+                while (!resp.equals("end!")) {
+                    resp = in.readLine();
+                    System.out.println(resp);
+                    if (resp.contains("name")) {
+                        System.out.println(name);
+                        out.println(name);
+                    }
+                    if (resp.contains("old")) {
+                        System.out.println(age);
+                        out.println(age);
+                    }
+                    if (resp.contains("game")) {
+                        System.out.println(choice);
+                        out.println(choice);
+                    }
+                }
+            } catch (NullPointerException npe) {
+                npe.printStackTrace();
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
--- src/main/java/ServerApp.java
+++ src/main/java/ServerApp.java
@@ -15,10 +15,48 @@ public class ServerApp {
              BufferedReader in = new BufferedReader(
                      new InputStreamReader(socket.getInputStream()))) {
             System.out.println("New connection accepted");
+            out.println("Write your name please:");
 
             final String name = in.readLine();
 
-            out.println(String.format("Hi %s, your port is %d", name, socket.getPort()));
+
+            out.println(String.format("Hi %s, how old are you?", name));
+
+            try {
+                final int age = Integer.parseInt(in.readLine());
+                if (age < 18) {
+                    out.println(String.format("Welcome to the kids area, %s! Let's watch cartoons!", name));
+                    out.println("end!");
+                } else if ((age >= 18) && (age <= 69)) {
+                    out.println(String.format("Welcome to the adults area, %s! Let's play a game!))) Chose a number from 1 to 5!", name));
+                    int choice = Integer.parseInt(in.readLine());
+                    switch (choice) {
+                        case 1:
+                            out.println("Thank you for your choice! You are watching BDSM porno now!");
+                            out.println("end!");
+                        case 2:
+                            out.println("Thank you for your choice! You are watching RickRoll now!");
+                            out.println("end!");
+                        case 3:
+                            out.println("Thank you for your choice! You are watching BesogonTV now!");
+                            out.println("end!");
+                        case 4:
+                            out.println("Thank you for your choice! You are watching SolovievLive now!");
+                            out.println("end!");
+                        case 5:
+                            out.println("Thank you for your choice! You are watching Press Conference of President of the Russian Federation Vladimir Vladimirovich Putin now!");
+                            out.println("end!");
+                        case default:
+                            out.println("Are you kidding me?");
+                            out.println("end!");
+                    }
+                } else {
+                    out.println(String.format("Dear, %s! Your are too aged! Please take your pills or you'll get asskicked.", name));
+                    out.println("end!");
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
 
         } catch (IOException e) {
             e.printStackTrace();
